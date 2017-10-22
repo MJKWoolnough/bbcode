e814dbfd70228c01f7b87ef15c92b60489e3f3db
Michael Woolnough <michael.woolnough@gmail.com>
2017-10-22 09:05:05 +0100

    added code tag

---
+++ bbcodehtml/code.go
@@ -0,0 +1,20 @@
+package bbcodehtml
+
+import "github.com/MJKWoolnough/bbcode"
+
+type code struct{}
+
+func (code) Name() string {
+	return "code"
+}
+
+var (
+	codeOpen  = []byte("<pre>")
+	codeClose = []byte("</pre>")
+)
+
+func (code) Handle(p *bbcode.Processor, _ string) {
+	p.Write(codeOpen)
+	p.Print(p.GetContents("code"))
+	p.Write(codeClose)
+}

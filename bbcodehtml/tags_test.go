74cf3154253d554526199557c29b0092753aa741
Michael Woolnough <michael.woolnough@gmail.com>
2025-10-12 16:56:09 +0100

    reformatted

--- bbcodehtml/tags_test.go
+++ bbcodehtml/tags_test.go
@@ -13,15 +13,17 @@ type inout struct {
 
 func testTag(t *testing.T, tests []inout, types ...bbcode.Handler) {
 	t.Parallel()
+
 	b := bbcode.New(types...)
+
 	var buf bytes.Buffer
+
 	for n, test := range tests {
 		buf.Reset()
+
 		if err := b.ConvertString(&buf, test.Input); err != nil {
 			t.Errorf("test %d: unexpected error: %s", n+1, err)
-			continue
-		}
-		if res := buf.String(); res != test.Output {
+		} else if res := buf.String(); res != test.Output {
 			t.Errorf("test %d: expecting %q, got %q", n+1, test.Output, res)
 		}
 	}
@@ -34,6 +36,7 @@ type attrinout struct {
 
 func testAttr(t *testing.T, tests []attrinout) {
 	t.Parallel()
+
 	for n, test := range tests {
 		if output := Colours.AttrFilter(test.Input); !bytes.Equal(test.Output, output) {
 			t.Errorf("test %d: expecting %s, got %s", n+1, test.Output, output)
